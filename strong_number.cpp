69bcfe0d5cb923de86ceae5075edb7b4dd69c95f
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-05-09 22:49:38 +0530

    Update and rename Strong_number.cpp to strong_number.cpp

--- strong_number.cpp
+++ strong_number.cpp
@@ -1,26 +1,20 @@
 #include <stdio.h>
-
-// Function to calculate the factorial of a number
 int factorial(int n) {
     if (n == 0 || n == 1) 
         return 1;
     else 
         return n * factorial(n - 1);
 }
-
-// Function to check if a number is strong or not
 int isStrong(int num) {
     int originalNum = num;
     int sum = 0;
 
-    // Calculate sum of factorials of digits
     while (num > 0) {
         int digit = num % 10;
         sum += factorial(digit);
         num /= 10;
     }
 
-    // Check if the number is strong
     return (sum == originalNum);
 }
 
