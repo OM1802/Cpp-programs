e70b6dc737bbd57c6ed6322c810b6f36dd847e59
OM_JAISWAL <omjaiswal1802@gmail.com>
2024-05-27 22:23:37 +0530

    Update gst.cpp

--- gst.cpp
+++ gst.cpp
@@ -1,22 +1,15 @@
+//CPP code to calculate G.S.T on the total of a bill
 #include <iostream>
 using namespace std;
 
 int main() {
-    // Declare variables
     double originalAmount, gstRate, gstAmount, totalAmount;
-
-    // Initialize GST rate (18%)
     gstRate = 18.0;
 
-    // Prompt the user to enter the original bill amount
+    
     cout << "Enter the original bill amount: ";
     cin >> originalAmount;
-
-    // Calculate the GST amount
     gstAmount = (originalAmount * gstRate) / 100;
-
-    // Calculate the total amount including GST
     totalAmount = originalAmount + gstAmount;
-
-    // Display the results
+    cout << totalAmount;
 
